64f64627ae2177f327d3da851e153209cfb11db3
lipipatel27-cpu <lipipatel27@gmail.com>
2026-03-01 15:12:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 'use strict';
-module.exports = require('./database');
-module.exports.SqliteError = require('./sqlite-error');
+
+var bind = require('function-bind');
+var $TypeError = require('es-errors/type');
+
+var $call = require('./functionCall');
+var $actualApply = require('./actualApply');
+
+/** @type {(args: [Function, thisArg?: unknown, ...args: unknown[]]) => Function} TODO FIXME, find a way to use import('.') */
+module.exports = function callBindBasic(args) {
+	if (args.length < 1 || typeof args[0] !== 'function') {
+		throw new $TypeError('a function is required');
+	}
+	return $actualApply(bind, $call, args);
+};
--- utils.js
+++ utils.js
@@ -1,57 +1,98 @@
-'use strict';
+'use strict'
 
-exports.repeat = function (str, num) {
-  var result = '';
-  for (var i = 0; i < num; i++) { result += str; }
-  return result;
-};
+/**
+ * Module dependencies.
+ */
 
-exports.arrayEqual = function (a, b) {
-  if (a.length !== b.length) { return false; }
-  for (var i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) { return false; }
-  }
-  return true;
-};
+var bytes = require('bytes')
+var contentType = require('content-type')
+var typeis = require('type-is')
 
-exports.trimChars = function (str, chars) {
-  var start = 0;
-  var end = str.length - 1;
-  while (chars.indexOf(str.charAt(start)) >= 0) { start++; }
-  while (chars.indexOf(str.charAt(end)) >= 0) { end--; }
-  return str.slice(start, end + 1);
-};
-
-exports.capitalize = function (str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-};
-
-exports.arrayUnion = function () {
-  var result = [];
-  for (var i = 0, values = {}; i < arguments.length; i++) {
-    var arr = arguments[i];
-    for (var j = 0; j < arr.length; j++) {
-      if (!values[arr[j]]) {
-        values[arr[j]] = true;
-        result.push(arr[j]);
-      }
-    }
-  }
-  return result;
-};
-
-function has(obj, key) {
-  return Object.prototype.hasOwnProperty.call(obj, key);
+/**
+ * Module exports.
+ */
+module.exports = {
+  getCharset,
+  normalizeOptions,
+  passthrough
 }
 
-exports.has = has;
-
-exports.extend = function (dest, src) {
-  for (var i in src) {
-    if (has(src, i)) { dest[i] = src[i]; }
+/**
+ * Get the charset of a request.
+ *
+ * @param {Object} req
+ * @returns {string | undefined}
+ * @private
+ */
+function getCharset (req) {
+  try {
+    return (contentType.parse(req).parameters.charset || '').toLowerCase()
+  } catch {
+    return undefined
   }
-};
+}
 
-exports.trimEnd = function (str) {
-  return str.replace(/\s+$/g, '');
-};
+/**
+ * Get the simple type checker.
+ *
+ * @param {string | string[]} type
+ * @returns {Function}
+ * @private
+ */
+function typeChecker (type) {
+  return function checkType (req) {
+    return Boolean(typeis(req, type))
+  }
+}
+
+/**
+ * Normalizes the common options for all parsers.
+ *
+ * @param {Object} options options to normalize
+ * @param {string | string[] | Function} defaultType default content type(s) or a function to determine it
+ * @returns {Object}
+ * @private
+ */
+function normalizeOptions (options, defaultType) {
+  if (!defaultType) {
+    // Parsers must define a default content type
+    throw new TypeError('defaultType must be provided')
+  }
+
+  var inflate = options?.inflate !== false
+  var limit = typeof options?.limit !== 'number'
+    ? bytes.parse(options?.limit || '100kb')
+    : options?.limit
+  var type = options?.type || defaultType
+  var verify = options?.verify || false
+  var defaultCharset = options?.defaultCharset || 'utf-8'
+
+  if (verify !== false && typeof verify !== 'function') {
+    throw new TypeError('option verify must be function')
+  }
+
+  // create the appropriate type checking function
+  var shouldParse = typeof type !== 'function'
+    ? typeChecker(type)
+    : type
+
+  return {
+    inflate,
+    limit,
+    verify,
+    defaultCharset,
+    shouldParse
+  }
+}
+
+/**
+ * Passthrough function that returns input unchanged.
+ * Used by parsers that don't need to transform the data.
+ *
+ * @param {*} value
+ * @returns {*}
+ * @private
+ */
+function passthrough (value) {
+  return value
+}
